7233b52c6e8f5cc083197ecf277b4450bf640adb
Adam <adam.hall@sky.uk>
2018-03-06 03:00:35 +0000

    added reviews query

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,7 +1,7 @@
 import limit from './queries/limit.graphql';
-import reviews from './queries/reviews.graphql';
+import reviewsQuery from './queries/reviews.graphql';
 
 export {
     limit,
-    reviews,
+    reviewsQuery,
 };
