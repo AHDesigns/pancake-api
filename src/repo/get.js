56fa60a8b11f9c36ba809c15d1993116a5cde323
Adam Hall <adamhalldesigns@gmail.com>
2019-07-31 08:13:09 +0100

    add socket and repo info

---
+++ src/repo/get.js
@@ -0,0 +1,11 @@
+module.exports = cache => (_, res, next) => {
+    try {
+        res.json(
+            Object.values(
+                cache.all(),
+            ).map(({ params }) => params),
+        );
+    } catch (e) {
+        next(e);
+    }
+};
